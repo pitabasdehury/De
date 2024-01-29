381417b7e98e762e137727421e070e24acd5b982
Knowledge Hub <pitabasdehury@gmail.com>
2024-01-29 02:19:59 +0000

    1st

---
+++ 01.sh
@@ -0,0 +1,13 @@
+# Write a Shell Script to add two numbers ?
+
+#!/bin/bash
+
+# Store two numbers in variables
+   a=2
+   b=3
+
+  echo The numbers are $a '&' $b     # Print the numbers
+
+  c=$((a + b))                     # perform addition
+
+  echo $a + $b = $c                # printing addition value
